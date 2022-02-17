96bfdd2e0cd522b1be30a775cc545f79dcd94d72
Rémi Bardon <remi.bardon.dev@gmail.com>
2022-02-17 21:43:48 +0100

    Replace hard-coded colors by color sets in Assets This allows dynamic appearance (dark mode, high contrast…)

--- Prose/Prose/Extensions/Color+Extension.swift
+++ Prose/Prose/Extensions/Color+Extension.swift
@@ -10,77 +10,77 @@ import SwiftUI
 extension Color {
     // States
     public static var stateGreen: Color {
-        return Color(red: 0 / 255.0, green: 171.0 / 255.0, blue: 126.0 / 255.0)
+        return Color("state/green")
     }
     
     public static var stateGreenLight: Color {
-        return Color(red: 97.0 / 255.0, green: 199.0 / 255.0, blue: 84.0 / 255.0)
+        return Color("state/greenLight")
     }
     
     public static var stateBlue: Color {
-        return Color(red: 0 / 255.0, green: 98.0 / 255.0, blue: 233.0 / 255.0)
+        return Color("state/blue")
     }
     
     public static var stateGrey: Color {
-        return Color(red: 121.0 / 255.0, green: 121.0 / 255.0, blue: 121.0 / 255.0)
+        return Color("state/grey")
     }
     
     public static var stateGreyLight: Color {
-        return Color(red: 187.0 / 255.0, green: 187.0 / 255.0, blue: 187.0 / 255.0)
+        return Color("state/greyLight")
     }
     
     // Borders
     public static var borderPrimary: Color {
-        return Color(red: 131.0 / 255.0, green: 131.0 / 255.0, blue: 131.0 / 255.0)
+        return Color("border/primary")
     }
     
     public static var borderSecondary: Color {
-        return Color(red: 189.0 / 255.0, green: 189.0 / 255.0, blue: 189.0 / 255.0)
+        return Color("border/secondary")
     }
     
     public static var borderTertiary: Color {
-        return Color(red: 230.0 / 255.0, green: 230.0 / 255.0, blue: 230.0 / 255.0)
+        return Color("border/tertiary")
     }
     
     public static var borderTertiaryLight: Color {
-        return Color(red: 211.0 / 255.0, green: 211.0 / 255.0, blue: 211.0 / 255.0)
+        return Color("border/tertiaryLight")
     }
     
     // Texts
     public static var textPrimary: Color {
-        return Color(red: 35.0 / 255.0, green: 37.0 / 255.0, blue: 38.0 / 255.0)
+        return Color("text/primary")
     }
     
     public static var textPrimaryLight: Color {
-        return Color(red: 77.0 / 255.0, green: 77.0 / 255.0, blue: 77.0 / 255.0)
+        return Color("text/primaryLight")
     }
     
     public static var textSecondary: Color {
-        return Color(red: 121.0 / 255.0, green: 121.0 / 255.0, blue: 121.0 / 255.0)
+        return Color("text/secondary")
     }
     
     public static var textSecondaryLight: Color {
-        return Color(red: 128.0 / 255.0, green: 128.0 / 255.0, blue: 128.0 / 255.0)
+        return Color("text/secondaryLight")
     }
     
     // Buttons
     public static var buttonPrimary: Color {
-        return Color(red: 0.0 / 255.0, green: 98.0 / 255.0, blue: 233.0 / 255.0)
+        return Color("button/primary")
     }
     
     public static var buttonActionGradientFromText: Color {
-        return Color(red: 39.0 / 255.0, green: 125.0 / 255.0, blue: 255.0 / 255.0)
+        return Color("button/actionGradientFromText")
     }
     
     public static var buttonActionGradientToText: Color {
-        return Color(red: 1.0 / 255.0, green: 100.0 / 255.0, blue: 255.0 / 255.0)
+        return Color("button/actionGradientToText")
     }
     
     public static var buttonActionText: Color {
-        return Color(red: 0.0 / 255.0, green: 98.0 / 255.0, blue: 233.0 / 255.0)
+        return Color("button/actionText")
     }
     
     public static var buttonActionShadow: Color {
-        return Color(red: 0.0 / 255.0, green: 93.0 / 255.0, blue: 233.0 / 255.0)
+        return Color("button/actionShadow")
     }
 }
