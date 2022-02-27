3bbd92610ae9a2a74ce4097ebd1da2777322c248
Rémi Bardon <remi.bardon.dev@gmail.com>
2022-02-27 09:44:12 +0100

    Improve details sidebar code (split into functions)

--- Prose/Prose/Views/Message/MessageDetailsView.swift
+++ Prose/Prose/Views/Message/MessageDetailsView.swift
@@ -8,74 +8,74 @@
 import SwiftUI
 
 struct MessageDetailsView: View {
+    private static let titleSpacing: CGFloat = 9
+    private static let sidesPadding: CGFloat = 15
+    
     var avatar: String
     var name: String
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack {
             ScrollView {
                 VStack(spacing: 10) {
-                    Spacer()
-                    
-                    VStack(spacing: 3) {
+                    VStack(spacing: 12) {
                         ContentMessageDetailsIdentityComponent(
                             avatar: avatar,
                             name: name
                         )
-                        
-                        Spacer()
-                        
                         ContentMessageDetailsQuickActionsComponent()
                     }
+                    .padding(.vertical)
                     
-                    Spacer()
-                        .frame(height: 2)
-                     
                     VStack(spacing: 22) {
-                        let titleSpacing: CGFloat = 9
-                        let paddingSides: CGFloat = 15
-                        
-                        VStack(alignment: .leading, spacing: titleSpacing) {
-                            // Information
-                            ContentMessageDetailsTitleComponent(
-                                title: "content_message_details_information_title".localized(),
-                                paddingSides: paddingSides
-                            )
-                            
-                            ContentMessageDetailsInformationComponent()
-                                .padding(.horizontal, paddingSides)
-                        }
-                        
-                        // Security
-                        VStack(alignment: .leading, spacing: titleSpacing) {
-                            ContentMessageDetailsTitleComponent(
-                                title: "content_message_details_security_title".localized(),
-                                paddingSides: paddingSides
-                            )
-                            
-                            ContentMessageDetailsSecurityComponent()
-                                .padding(.horizontal, paddingSides)
-                        }
-                        
-                        // Actions
-                        VStack(alignment: .leading, spacing: titleSpacing - 3) {
-                            let paddingActionsSides: CGFloat = 6
-                            
-                            ContentMessageDetailsTitleComponent(
-                                title: "content_message_details_actions_title".localized(),
-                                paddingSides: paddingSides
-                            )
-                            
-                            ContentMessageDetailsActionsComponent(
-                                paddingSides: paddingSides - paddingActionsSides
-                            )
-                                .padding(.horizontal, paddingActionsSides)
-                        }
+                        informationSection()
+                        securitySection()
+                        actionsSection()
                     }
                 }
             }
         }
         .background(.background)
+    }
+    
+    private func informationSection() -> some View {
+        VStack(alignment: .leading, spacing: Self.titleSpacing) {
+            ContentMessageDetailsTitleComponent(
+                title: "content_message_details_information_title".localized(),
+                paddingSides: Self.sidesPadding
+            )
+            
+            ContentMessageDetailsInformationComponent()
+                .padding(.horizontal, Self.sidesPadding)
+        }
+    }
+    
+    private func securitySection() -> some View {
+        VStack(alignment: .leading, spacing: Self.titleSpacing) {
+            ContentMessageDetailsTitleComponent(
+                title: "content_message_details_security_title".localized(),
+                paddingSides: Self.sidesPadding
+            )
+            
+            ContentMessageDetailsSecurityComponent()
+                .padding(.horizontal, Self.sidesPadding)
+        }
+    }
+    
+    private func actionsSection() -> some View {
+        VStack(alignment: .leading, spacing: Self.titleSpacing - 3) {
+            let paddingActionsSides: CGFloat = 6
+            
+            ContentMessageDetailsTitleComponent(
+                title: "content_message_details_actions_title".localized(),
+                paddingSides: Self.sidesPadding
+            )
+            
+            ContentMessageDetailsActionsComponent(
+                paddingSides: Self.sidesPadding - paddingActionsSides
+            )
+                .padding(.horizontal, paddingActionsSides)
+        }
     }
 }
 
