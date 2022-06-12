843ec7d8a77999d70ba8828697e1736d51711b07
Marc Bauer <mb@nesium.com>
2022-06-12 18:04:27 +0200

    chore: Add build tools (#29)

---
+++ BuildTools/Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "BuildTools",
+    platforms: [.macOS(.v10_11)],
+    dependencies: [
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.49.9"),
+        .package(url: "https://github.com/SwiftGen/SwiftGen", from: "6.5.1"),
+    ],
+    targets: [.target(name: "BuildTools", path: "")]
+)
