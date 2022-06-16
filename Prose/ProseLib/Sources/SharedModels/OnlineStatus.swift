97f0da1d7dacb37a90c67608402bc789e074c5b1
Rémi Bardon <remi.bardon.dev@gmail.com>
2022-06-16 11:47:23 +0200

    feat: Add sidebar account popover (#33)

--- Prose/ProseLib/Sources/SharedModels/OnlineStatus.swift
+++ Prose/ProseLib/Sources/SharedModels/OnlineStatus.swift
@@ -2,6 +2,28 @@ public enum OnlineStatus: Hashable, CaseIterable {
     case offline, online
 }
 
+public extension OnlineStatus {
+    var localizedDescription: String {
+        switch self {
+        case .offline:
+            return "Offline"
+        case .online:
+            return "Online"
+        }
+    }
+}
+
 public enum Availability: Hashable, CaseIterable {
     case available, doNotDisturb
+}
+
+public extension Availability {
+    var localizedDescription: String {
+        switch self {
+        case .available:
+            return "Available for chat"
+        case .doNotDisturb:
+            return "Do not disturb"
+        }
+    }
 }
--- Prose/ProseLib/Sources/SidebarFeature/Footer/FooterAvatar.swift
+++ Prose/ProseLib/Sources/SidebarFeature/Footer/FooterAvatar.swift
@@ -26,28 +26,153 @@ struct FooterAvatar: View {
     var body: some View {
         WithViewStore(self.store) { viewStore in
             Button(action: { actions.send(.avatarTapped) }) {
-                Avatar(viewStore.avatar, size: viewStore.size)
+                Avatar(viewStore.avatar, size: 32)
             }
             .buttonStyle(.plain)
             .accessibilityLabel(l10n.label)
             .overlay(alignment: .bottomTrailing) {
-                statusIndicator(viewStore: viewStore)
+                AvailabilityIndicator(availability: viewStore.availability)
                     // Offset of half the size minus 2 points (otherwise it looks odd)
                     .alignmentGuide(.trailing) { d in d.width / 2 + 2 }
                     .alignmentGuide(.bottom) { d in d.height / 2 + 2 }
             }
+            .popover(isPresented: viewStore.binding(\State.$showingPopover), content: popover)
         }
     }
 
-    @ViewBuilder
-    private func statusIndicator(viewStore: ViewStore<State, Action>) -> some View {
-        if viewStore.status != .offline {
-            OnlineStatusIndicator(status: viewStore.status)
-                .padding(2)
-                .background {
-                    Circle()
-                        .fill(Color.white)
+    private func popover() -> some View {
+        Self.popover(store: self.store)
+    }
+
+    fileprivate static func popover(store: Store<State, Action>) -> some View {
+        WithViewStore(store) { viewStore in
+            VStack(alignment: .leading, spacing: 16) {
+                // TODO: [Rémi Bardon] Refactor this view out
+                HStack {
+                    // TODO: [Rémi Bardon] Change this to Crisp icon
+                    Avatar(PreviewImages.Avatars.baptiste.rawValue, size: 32)
+                    VStack(alignment: .leading) {
+                        Text(verbatim: viewStore.fullName)
+                            .font(.headline)
+                        Text(verbatim: viewStore.jid)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                    .foregroundColor(.primary)
                 }
+                // Make hit box full width
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .contentShape(Rectangle())
+                .accessibilityElement(children: .ignore)
+                .accessibilityLabel(l10n.Header.label(viewStore.fullName, viewStore.jid))
+
+                GroupBox {
+                    Button { viewStore.send(.updateMoodTapped) } label: {
+                        HStack(spacing: 4) {
+                            Text(String(viewStore.statusIcon))
+                                .accessibilityHidden(true)
+                            Text(verbatim: l10n.UpdateMood.title)
+                        }
+                        .disclosureIndicator()
+                    }
+                    Menu(l10n.ChangeAvailability.title) {
+                        Self.availabilityMenu(store: store)
+                    }
+                    // NOTE: [Rémi Bardon] This inverted padding fixes the padding SwiftUI adds for `Menu`s.
+                    .padding(.leading, -3)
+                    // NOTE: [Rémi Bardon] Having the disclosure indicator outside the menu label
+                    //       reduces the hit box, but we can't have it inside, otherwise SwiftUI
+                    //       places the `Image` on the leading edge.
+                    .disclosureIndicator()
+                    Button { viewStore.send(.pauseNotificationsTapped) } label: {
+                        Text(verbatim: l10n.PauseNotifications.title)
+                            .disclosureIndicator()
+                    }
+                }
+                GroupBox {
+                    Button(l10n.EditProfile.title) { viewStore.send(.editProfileTapped) }
+                    Button(l10n.AccountSettings.title) { viewStore.send(.accountSettingsTapped) }
+                }
+                GroupBox {
+                    Button { viewStore.send(.offlineModeTapped) } label: {
+                        Text(verbatim: l10n.OfflineMode.title)
+                            .disclosureIndicator()
+                    }
+                }
+                GroupBox {
+                    Button(l10n.SignOut.title, role: .destructive) { viewStore.send(.signOutTapped) }
+                }
+            }
+            .menuStyle(.borderlessButton)
+            .menuIndicator(.hidden)
+            .buttonStyle(SidebarFooterPopoverButtonStyle())
+            .groupBoxStyle(VStackGroupBoxStyle(alignment: .leading, spacing: 6))
+            .multilineTextAlignment(.leading)
+            .padding(12)
+            .frame(width: 196)
+        }
+    }
+
+    static func availabilityMenu(store: Store<State, Action>) -> some View {
+        WithViewStore(store) { viewStore in
+            ForEach(Availability.allCases, id: \.self) { availability in
+                Button { viewStore.send(.changeAvailabilityTapped(availability)) } label: {
+                    HStack {
+                        // NOTE: [Rémi Bardon] We could use a `Label` or `HStack` here,
+                        //       to add the colored dot, but `Menu`s don't display it.
+                        Text(availability.localizedDescription)
+                        if viewStore.availability == availability {
+                            Spacer()
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+                .tag(availability)
+                .disabled(viewStore.availability == availability)
+            }
+        }
+    }
+}
+
+struct VStackGroupBoxStyle: GroupBoxStyle {
+    let alignment: HorizontalAlignment
+    let spacing: CGFloat?
+
+    func makeBody(configuration: Configuration) -> some View {
+        VStack(alignment: self.alignment, spacing: self.spacing) { configuration.content }
+    }
+}
+
+struct SidebarFooterPopoverButtonStyle: ButtonStyle {
+    @Environment(\.isEnabled) private var isEnabled
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .opacity((configuration.isPressed || !self.isEnabled) ? 0.5 : 1)
+            .foregroundColor(Self.color(for: configuration.role))
+            // Make hit box full width
+            // NOTE: [Rémi Bardon] We could avoid making the hit box full width for destructive actions.
+            .frame(maxWidth: .infinity, alignment: .leading)
+            // Allow hits in the transparent areas
+            .contentShape(Rectangle())
+    }
+
+    static func color(for role: ButtonRole?) -> Color? {
+        switch role {
+        case .some(.destructive):
+            return .red
+        default:
+            return nil
+        }
+    }
+}
+
+extension View {
+    func disclosureIndicator() -> some View {
+        HStack(spacing: 4) {
+            self
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Image(systemName: "chevron.forward")
+                .padding(.trailing, 2)
         }
     }
 }
@@ -60,40 +185,60 @@ public let footerAvatarReducer: Reducer<
     FooterAvatarState,
     FooterAvatarAction,
     Void
-> = Reducer { _, action, _ in
+> = Reducer { state, action, _ in
     switch action {
     case .avatarTapped:
-        print("Avatar tapped")
+        state.showingPopover = true
+
+    case let .changeAvailabilityTapped(availability):
+        state.availability = availability
+
+    case .binding:
+        break
 
     default:
         // TODO: [Rémi Bardon] Handle actions
-        assertionFailure("Received unhandled action: \(String(describing: action))")
+        print("Received unhandled action: \(String(describing: action))")
     }
 
     return .none
-}
+}.binding()
 
 // MARK: State
 
 public struct FooterAvatarState: Equatable {
-    let avatar: String
-    let status: OnlineStatus
-    let size: CGFloat
+    var avatar: String
+    var availability: Availability
+    var fullName: String
+    var jid: String
+    var statusIcon: Character
+    var statusMessage: String
+
+    @BindableState var showingPopover: Bool
 
     public init(
         avatar: String,
-        status: OnlineStatus = .offline,
-        size: CGFloat = 32
+        availability: Availability = .available,
+        fullName: String = "Baptiste Jamin",
+        jid: String = "baptiste@crisp.chat",
+        statusIcon: Character = "🚀",
+        statusMessage: String = "Building new features.",
+        showingPopover: Bool = false
     ) {
         self.avatar = avatar
-        self.status = status
-        self.size = size
+        self.availability = availability
+        self.showingPopover = showingPopover
+        self.fullName = fullName
+        self.jid = jid
+        self.statusIcon = statusIcon
+        self.statusMessage = statusMessage
+        self.showingPopover = showingPopover
     }
 }
 
 // MARK: Actions
 
-public enum FooterAvatarAction: Equatable {
+public enum FooterAvatarAction: Equatable, BindableAction {
     case avatarTapped
     case updateMoodTapped
     case changeAvailabilityTapped(Availability)
@@ -102,6 +247,7 @@ public enum FooterAvatarAction: Equatable {
     case accountSettingsTapped
     case offlineModeTapped
     case signOutTapped
+    case binding(BindingAction<FooterAvatarState>)
 }
 
 // MARK: - Previews
@@ -113,14 +259,28 @@ public enum FooterAvatarAction: Equatable {
         private struct Preview: View {
             var body: some View {
                 HStack {
-                    ForEach(OnlineStatus.allCases, id: \.self) { status in
+                    ForEach(Availability.allCases, id: \.self) { availability in
                         content(state: FooterAvatarState(
                             avatar: PreviewImages.Avatars.valerian.rawValue,
-                            status: status
+                            availability: availability
                         ))
                     }
                 }
                 .padding()
+                let store = Store(
+                    initialState: FooterAvatarState(
+                        avatar: PreviewImages.Avatars.valerian.rawValue,
+                        availability: .available
+                    ),
+                    reducer: footerAvatarReducer,
+                    environment: ()
+                )
+                FooterAvatar.popover(store: store)
+                VStack(alignment: .leading) {
+                    FooterAvatar.availabilityMenu(store: store)
+                }
+                .padding()
+                .buttonStyle(.plain)
             }
 
             private func content(state: FooterAvatarState) -> some View {
