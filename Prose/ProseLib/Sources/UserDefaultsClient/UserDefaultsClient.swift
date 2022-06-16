cafb76e6c65e1ca9f212d8bb55dfe7f7a66f6a84
Rémi Bardon <remi.bardon.dev@gmail.com>
2022-06-16 13:23:17 +0200

    feat: Implement sign out button

--- Prose/ProseLib/Sources/UserDefaultsClient/UserDefaultsClient.swift
+++ Prose/ProseLib/Sources/UserDefaultsClient/UserDefaultsClient.swift
@@ -11,25 +11,32 @@ import SharedModels
 public struct UserDefaultsClient {
     public var loadCurrentAccount: () -> JID?
     public var saveCurrentAccount: (_ jid: JID) -> Void
+    public var deleteCurrentAccount: () -> Void
 }
 
 public extension UserDefaultsClient {
     static var placeholder: UserDefaultsClient {
         UserDefaultsClient(
             loadCurrentAccount: { nil },
-            saveCurrentAccount: { _ in () }
+            saveCurrentAccount: { _ in () },
+            deleteCurrentAccount: {}
         )
     }
 }
 
 public extension UserDefaultsClient {
+    internal static let jidKey = "jid"
+
     static func live(_ defaults: UserDefaults) -> Self {
         Self(
             loadCurrentAccount: {
-                defaults.string(forKey: "jid").flatMap(JID.init(rawValue:))
+                defaults.string(forKey: Self.jidKey).flatMap(JID.init(rawValue:))
             },
             saveCurrentAccount: { jid in
-                defaults.set(jid.jidString, forKey: "jid")
+                defaults.set(jid.jidString, forKey: Self.jidKey)
+            },
+            deleteCurrentAccount: {
+                defaults.removeObject(forKey: Self.jidKey)
             }
         )
     }
