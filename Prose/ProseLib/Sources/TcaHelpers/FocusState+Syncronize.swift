e9b87fa2921274bf7cee9a7939a749750a235039
Rémi Bardon <remi.bardon.dev@gmail.com>
2022-06-22 13:56:56 +0200

    chore: Fix "Redundant conformance constraint 'Value' : 'Equatable'"

--- Prose/ProseLib/Sources/TcaHelpers/FocusState+Syncronize.swift
+++ Prose/ProseLib/Sources/TcaHelpers/FocusState+Syncronize.swift
@@ -4,7 +4,7 @@ public extension View {
     /// Synchronizes a `Binding` and a `FocusState.Binding`.
     ///
     /// Comes from <https://github.com/pointfreeco/episode-code-samples/blob/891f028d38bfa9f1bbbd3b4e24760feaa43947fe/0159-safer-conciser-forms-pt2/ConciseForms/ConciseForms/FocusState.swift#L70-L77>.
-    func synchronize<Value: Equatable>(
+    func synchronize<Value>(
         _ first: Binding<Value>,
         _ second: FocusState<Value>.Binding
     ) -> some View {
