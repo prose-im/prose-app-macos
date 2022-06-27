45ae90be1248fe0684eab54dd1dc49cd4a188979
mb <mb@nesium.com>
2022-06-27 12:44:11 +0200

    chore: Replace force-unwrap with fatalError

--- Prose/ProseLib/Tests/ProseCoreTCATests/Helpers/BareJid+Tests.swift
+++ Prose/ProseLib/Tests/ProseCoreTCATests/Helpers/BareJid+Tests.swift
@@ -1,8 +1,9 @@
 import Foundation
 import ProseCoreClientFFI
+import Toolbox
 
 extension BareJid: ExpressibleByStringLiteral {
     public init(stringLiteral value: String) {
-        self = try! ProseCoreClientFFI.parseJid(jid: value)
+        self = (try? ProseCoreClientFFI.parseJid(jid: value)).expect("Invalid JID.")
     }
 }
