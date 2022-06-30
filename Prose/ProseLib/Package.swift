506576c28ff7dfac1aea4f7a5524207cb9016969
Rémi Bardon <remi.bardon.dev@gmail.com>
2022-06-30 15:34:53 +0200

    chore: Load suggestions from TCA environment and support sections

--- Prose/ProseLib/Package.swift
+++ Prose/ProseLib/Package.swift
@@ -104,7 +104,10 @@ let package = Package(
         .product(name: "SwiftUINavigation", package: "swiftui-navigation"),
       ]
     ),
-    .target(name: "SearchSuggestionsFeature", dependencies: [.featureBase]),
+    .target(name: "SearchSuggestionsFeature", dependencies: [
+      "AutoSuggestClient",
+      .featureBase,
+    ]),
     .target(
       name: "UnreadFeature",
       dependencies: [
@@ -114,6 +117,8 @@ let package = Package(
     ),
 
     // MARK: Dependencies
+
+    .target(name: "AutoSuggestClient", dependencies: [.base]),
 
     .target(name: "CredentialsClient", dependencies: [.base]),
     .testTarget(name: "CredentialsClientTests", dependencies: ["CredentialsClient"]),
