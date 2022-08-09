66b064529c893d4d6df04650fdb9367cd538084a
Rémi Bardon <remi.bardon.dev@gmail.com>
2022-08-09 15:51:41 +0200

    chore: PR fix: Remove unnecessary `prose_toggle`s and document the remaining functions`

--- Prose/ProseLib/Sources/ProseCoreTCA/OrderedCollections+Ext.swift
+++ Prose/ProseLib/Sources/ProseCoreTCA/OrderedCollections+Ext.swift
@@ -5,31 +5,33 @@
 
 import OrderedCollections
 
-public extension Dictionary {
-  @discardableResult mutating func prose_toggle<Element>(
-    _ element: Element,
-    forKey key: Key
-  ) -> Bool where Value == OrderedSet<Element> {
-    let inserted = self[key, default: Value()].unordered.prose_toggle(element)
-    if !inserted, self[key]?.isEmpty == true {
-      self.removeValue(forKey: key)
-    }
-    return inserted
-  }
-}
-
 public extension OrderedDictionary {
-  @discardableResult mutating func prose_toggle<Element>(
-    _ element: Element,
-    forKey key: Key
-  ) -> Bool where Value: SetAlgebra, Value.Element == Element {
-    let inserted = self[key, default: Value()].prose_toggle(element)
-    if !inserted, self[key]?.isEmpty == true {
-      self.removeValue(forKey: key)
-    }
-    return inserted
-  }
-
+  /// Inserts a new element for the given key or removes it if already present.
+  ///
+  /// Example:
+  /// ```
+  /// var dict: OrderedDictionary<String, OrderedSet<Int>> = [
+  ///   "first": [1, 2, 3],
+  ///   "second": [3, 1],
+  /// ]
+  ///
+  /// dict.prose_toggle(2, forKey: "first") // false
+  /// print(dict) // ["first": [1, 3], "second": [3, 1]]
+  ///
+  /// dict.prose_toggle(2, forKey: "second") // true
+  /// print(dict) // ["first": [1, 3], "second": [3, 1, 2]]
+  ///
+  /// dict.prose_toggle(1, forKey: "second") // false
+  /// print(dict) // ["first": [1, 3], "second": [3, 2]]
+  ///
+  /// dict.prose_toggle(1, forKey: "second") // true
+  /// print(dict) // ["first": [1, 3], "second": [3, 2, 1]]
+  /// ```
+  ///
+  /// - Parameters:
+  ///   - element: An element to insert/remove.
+  ///   - key: The key in which to search for the element.
+  /// - Returns: `true` if the element was inserted, `false` if it was removed.
   @discardableResult mutating func prose_toggle<Element>(
     _ element: Element,
     forKey key: Key
