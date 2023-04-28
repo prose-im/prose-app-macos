4e532338affaef7d3d0e6e17dafc90cf2c89a706
mb <mb@nesium.com>
2023-04-28 11:43:14 +0200

    chore: Update core lib

--- Prose/ProseLib/Package.swift
+++ Prose/ProseLib/Package.swift
@@ -40,7 +40,7 @@ let package = Package(
     .package(url: "https://github.com/nesium/swift-common-utils", .upToNextMajor(from: "1.2.0")),
 
 //     .package(name: "ProseCoreFFI", path: "../../../../prose-wrapper-swift/Build/spm/ProseCoreFFI"),
-    .package(url: "https://github.com/prose-im/prose-wrapper-swift", exact: "0.11.0"),
+    .package(url: "https://github.com/prose-im/prose-wrapper-swift", exact: "0.12.0"),
   ],
   targets: [
     .target(
